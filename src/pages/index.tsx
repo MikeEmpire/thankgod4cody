487f3c7c29c8b00672c2725bf29cffe2ef75fa58
Michael Olie <mike@grolens.com>
2022-06-04 05:34:39 -0700

    added sound on page load (master)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,19 @@
 import * as React from "react";
 import { useSpring, animated } from "react-spring";
+import useSound from "use-sound";
 import { Link } from "gatsby";
 
 import "../styles/main.scss";
+import intro from "../assets/intro.mp3";
+
+const { useEffect } = React;
 
 // markup
 function IndexPage() {
+  const [play] = useSound(intro, { interrupt: true });
+  useEffect(() => {
+    play();
+  }, [play]);
   const props = useSpring({
     to: { opacity: 1 },
     from: { opacity: 0 },
@@ -21,11 +29,12 @@ function IndexPage() {
   return (
     <div className="background" id="main__container">
       <animated.main style={props}>
-        You’re formally invited to a private one time streaming session of ThankGod4Cody’s upcoming project
+        You’re formally invited to a private one time streaming session of
+        ThankGod4Cody’s upcoming project
         <br /> Keep Me In The Light.{" "}
       </animated.main>
       <animated.nav style={navProps}>
-        <Link to="/KMITL">Enter Here</Link>
+        <Link to="/KMITL">Enter The Light</Link>
       </animated.nav>
     </div>
   );
